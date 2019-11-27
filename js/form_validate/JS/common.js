b443aabe86110dab3ec9e72e1976f4bd0a483e11
leo <robertmessi910@gmail.com>
2019-11-28 00:36:12 +0900

    JS課題の簡易アプリ②フォームバリデーションの、修正したJSをpushしました

--- js/form_validate/JS/common.js
+++ js/form_validate/JS/common.js
@@ -1,25 +1,33 @@
 'use strict';
 (function () {
-  var form = document.getElementById("form");
-  var name = form.name.value;
-  var kana = form.kana.value;
-  var tel = form.tel.value;
-  var mail = form.mail.value;
   document.getElementById("submit").addEventListener("click", function (event) {
-    if (name.length === 0 || name === null) {
+    var form = document.getElementById("form");
+    var name = form.name.value;
+    var kana = form.kana.value;
+    var tel = form.tel.value;
+    var mail = form.mail.value;
+    document.getElementById("err-name").textContent = "";
+    document.getElementById("err-kana").textContent = "";
+    document.getElementById("err-tel").textContent = "";
+    document.getElementById("err-mail").textContent = "";
+    if (name === "") {
       event.preventDefault();
+      document.getElementById("err-name").classList.add("red");
       document.getElementById("err-name").textContent = "名前が未入力です。";
     }
-    if (!kana.match(/[\x01-\x7E]{1,}/)) {
+    if (!kana.match(/[^\x01-\x7E]{1,}/)) {
       event.preventDefault();
+      document.getElementById("err-kana").classList.add("red");
       document.getElementById("err-kana").textContent = "全角カナで入力して下さい。";
     }
-    if (!tel.match(/[^\d]{10,11}/)) {
+    if (!tel.match(/\d{10,11}/)) {
       event.preventDefault();
+      document.getElementById("err-tel").classList.add("red");
       document.getElementById("err-tel").textContent = "電話番号をハイフンなしで入力して下さい。";
     }
     if (!mail.match(/^[a-zA-Z0-9]{1}[a-zA-Z0-9\.\_\-]*@{1}[a-zA-Z0-9\.\_\-]{1,}\.[a-zA-Z0-9]{1,}$/)) {
       event.preventDefault();
+      document.getElementById("err-mail").classList.add("red");
       document.getElementById("err-mail").textContent = "メールアドレスの形式ではありません。";
     }
   })
